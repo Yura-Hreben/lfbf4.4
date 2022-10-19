4c9fe22aa15fd54a673d49b1ec83ca057754cc45
yrii <yural1rn@gmail.com>
2022-10-19 19:17:50 +0300

    laba4.4

---
+++ laba4.4/laba4.4.cpp
@@ -0,0 +1,45 @@
+﻿// Lab_04_4.cpp
+// < Гребень, Юрій >
+// Лабораторна робота № 3.2
+// Табуляція функції, заданої графіком.
+// Варіант 5
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, R, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "R = "; cin >> R;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x <= -2)
+			y = 1;
+		else
+			if (-2 < x && x <= 4)
+				y = -R;
+			else
+				if (4 < x && x <= 8 - R)
+					y = -R;
+				else
+					if (8 - R < x && x <= 8 + R)
+						y = sqrt(R * R - (x - R) * (x - R));
+					else
+						y = -R;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
